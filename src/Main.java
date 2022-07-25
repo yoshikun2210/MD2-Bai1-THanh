bb198abcfb2ee1b2ce31a92095b21f6d5089ed10
yoshinkun2210 <yoshimkun22@gmail.com>
2022-07-25 14:09:36 +0700

    baith

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date ngayHienTai = new Date();
+        System.out.println("Ngày hiện tại :"+ ngayHienTai);
+
+
+    }
+}
